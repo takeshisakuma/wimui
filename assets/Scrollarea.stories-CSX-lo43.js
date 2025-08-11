5b93daa3b13a887667994e253be1f9285e9234de
takeshisakuma <saude0518@gmail.com>
2025-08-12 01:42:03 +0900

    Updates

--- assets/Scrollarea.stories-CSX-lo43.js
+++
@@ -1,7 +0,0 @@
-import{j as l}from"./jsx-runtime-D_zvdyIk.js";import{u as i,P as r}from"./index-BFktqcFp.js";import"./iframe-BLZf1Q20.js";const s=({text:a,scrollAxis:n="y",maxHeight:o="10rem",...c})=>{const{t}=i();return l.jsx("div",{className:["overflow-y-scroll","custom-scrollbar"].join(" "),style:{maxHeight:o},children:t(a)})};s.propTypes={text:r.string.isRequired,scrollAxis:r.oneOf(["y"]),maxHeight:r.string};s.__docgenInfo={description:"",methods:[],displayName:"Scrollarea",props:{text:{required:!0,tsType:{name:"string"},description:"",type:{name:"string"}},scrollAxis:{required:!1,tsType:{name:"literal",value:'"y"'},description:"",defaultValue:{value:'"y"',computed:!1},type:{name:"enum",value:[{value:'"y"',computed:!1}]}},maxHeight:{required:!1,tsType:{name:"string"},description:"Maximum height of the scroll area",type:{name:"string"}}}};const d={title:"Component/Scrollarea",component:s,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{backgroundColor:{control:"color"}}},e={args:{text:"sampletextforscrollarea",scrollAxis:"y",maxHeight:"10rem"}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {
-    text: 'sampletextforscrollarea',
-    scrollAxis: \`y\`,
-    maxHeight: \`10rem\`
-  }
-}`,...e.parameters?.docs?.source}}};const x=["ScrollareaYaxis"];export{e as ScrollareaYaxis,x as __namedExportsOrder,d as default};
--- assets/react-18-uQaWK6m2.js
+++ assets/react-18-uQaWK6m2.js
@@ -1,4 +1,4 @@
-import{a as md,r as $c}from"./iframe-BLZf1Q20.js";import{r as Sd}from"./index-CduyYjpy.js";var rc={exports:{}},ne={},wc={exports:{}},Wc={};/**
+import{d as md,r as $c}from"./iframe-Bfhehd2V.js";import{r as Sd}from"./index-BaMME6N3.js";var rc={exports:{}},ne={},wc={exports:{}},Wc={};/**
  * @license React
  * scheduler.production.js
  *
