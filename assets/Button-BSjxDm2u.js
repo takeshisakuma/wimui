b33738e483fef92d76aa72892096b87befeb60bf
takeshisakuma <saude0518@gmail.com>
2026-01-30 00:23:20 +0900

    Updates

---
+++ assets/Button-BSjxDm2u.js
@@ -0,0 +1,8 @@
+import{j as t}from"./jsx-runtime-u17CrQMm.js";import{useMDXComponents as s}from"./index-ktpFCZZf.js";import{M as e,T as i,S as m,D as a,C as x,a as p,b as j}from"./blocks-DNTguYSi.js";import{B as c,L as u}from"./Button.stories-CpPia6mp.js";import"./Icon-DK4qDEzR.js";import{T as f}from"./T-CMGhqdRi.js";import"./iframe-eiiYkaZ-.js";import"./preload-helper-PPVm8Dsz.js";import"./index-BD5WUTpe.js";import"./index-Csl434AX.js";import"./index-sCZswWJS.js";import"./useTranslation-BEPvlZI2.js";function r(o){const n={hr:"hr",...s(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(e,{of:c}),`
+`,t.jsx(i,{}),`
+`,t.jsx(m,{children:t.jsx(f,{k:"doc_button_title"})}),`
+`,t.jsx(a,{}),`
+`,t.jsx(x,{of:u}),`
+`,t.jsx(p,{}),`
+`,t.jsx(n.hr,{}),`
+`,t.jsx(j,{})]})}function X(o={}){const{wrapper:n}={...s(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(r,{...o})}):r(o)}export{X as default};
---
+++ assets/formatter-EIJCOSYU-DxPez5yh.js
@@ -0,0 +1,1 @@
+import{f as m,r as o}from"./blocks-DNTguYSi.js";import{b as t}from"./iframe-eiiYkaZ-.js";import"./preload-helper-PPVm8Dsz.js";import"./index-BD5WUTpe.js";import"./jsx-runtime-u17CrQMm.js";import"./index-Csl434AX.js";var e=t(o(),1),c=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{c as formatter};
