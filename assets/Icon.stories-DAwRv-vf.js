cd6444b2458562450e07d65253b89b90e79594ec
takeshisakuma <saude0518@gmail.com>
2026-01-29 19:49:10 +0900

    Updates

---
+++ assets/Icon.stories-DAwRv-vf.js
@@ -0,0 +1,13 @@
+import{I as o}from"./Icon-D_qZPRKJ.js";import"./jsx-runtime-u17CrQMm.js";import"./index-DyKs52yL.js";import"./iframe-CT5k3Vxi.js";import"./preload-helper-PPVm8Dsz.js";const{fn:a}=__STORYBOOK_MODULE_TEST__,m={title:"Component/Atoms_Typography/Icon",component:o,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{color:{control:{type:"select"},options:["destructive","positive","caution","informative","primary","secondary","tertiary","disabled"]}},args:{onClick:a(),name:"CircleIcon",size:"medium",color:"destructive"}},e={args:{name:"SquareIcon",size:"medium",color:"primary"}},r={args:{name:"CircleIcon",size:"large",color:"destructive"}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    name: "SquareIcon",
+    size: "medium",
+    color: "primary" // プレフィックスなしに変更
+  }
+}`,...e.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    name: "CircleIcon",
+    size: "large",
+    color: "destructive" // プレフィックスなしに変更
+  }
+}`,...r.parameters?.docs?.source}}};const p=["MediumSquareIcon","MediumCircleIcon"];export{r as MediumCircleIcon,e as MediumSquareIcon,p as __namedExportsOrder,m as default};
