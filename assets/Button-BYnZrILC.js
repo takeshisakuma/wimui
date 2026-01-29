7597b92d2b1e7c1827fcc3664a31b48169039ecd
takeshisakuma <saude0518@gmail.com>
2026-01-29 20:44:09 +0900

    Updates

--- assets/Button-BYnZrILC.js
+++
@@ -1,8 +0,0 @@
-import{j as t}from"./jsx-runtime-u17CrQMm.js";import{useMDXComponents as s}from"./index-D2R6vkQi.js";import{M as e,T as i,S as m,D as a,C as p,a as x,b as j}from"./index-BqmNVlkO.js";import{B as c,L as u}from"./Button.stories-kOGSC4Ur.js";import"./Icon-D_qZPRKJ.js";import{T as f}from"./T-B1nsvP1o.js";import"./iframe-CT5k3Vxi.js";import"./preload-helper-PPVm8Dsz.js";import"./index-DLky3sur.js";import"./index-CfBXltcY.js";import"./index-CSUkwqFQ.js";import"./index-DyKs52yL.js";import"./useTranslation-CaX2E8o2.js";function r(o){const n={hr:"hr",...s(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(e,{of:c}),`
-`,t.jsx(i,{}),`
-`,t.jsx(m,{children:t.jsx(f,{k:"doc_button_title"})}),`
-`,t.jsx(a,{}),`
-`,t.jsx(p,{of:u}),`
-`,t.jsx(x,{}),`
-`,t.jsx(n.hr,{}),`
-`,t.jsx(j,{})]})}function g(o={}){const{wrapper:n}={...s(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(r,{...o})}):r(o)}export{g as default};
---
+++ assets/Icon.stories-BTQLvk_4.js
@@ -0,0 +1,13 @@
+import{I as o}from"./Icon-Ico6educ.js";import"./jsx-runtime-u17CrQMm.js";import"./index-BQp3Z88X.js";import"./iframe--4dbALut.js";import"./preload-helper-PPVm8Dsz.js";const{fn:a}=__STORYBOOK_MODULE_TEST__,m={title:"Component/Atoms_Typography/Icon",component:o,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{color:{control:{type:"select"},options:["destructive","positive","caution","informative","primary","secondary","tertiary","disabled"]}},args:{onClick:a(),name:"CircleIcon",size:"medium",color:"destructive"}},e={args:{name:"SquareIcon",size:"medium",color:"primary"}},r={args:{name:"CircleIcon",size:"large",color:"destructive"}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    name: "SquareIcon",
+    size: "medium",
+    color: "primary" // プレフィックスなしに変更
+  }
+}`,...e.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    name: "CircleIcon",
+    size: "large",
+    color: "destructive" // プレフィックスなしに変更
+  }
+}`,...r.parameters?.docs?.source}}};const p=["MediumSquareIcon","MediumCircleIcon"];export{r as MediumCircleIcon,e as MediumSquareIcon,p as __namedExportsOrder,m as default};
