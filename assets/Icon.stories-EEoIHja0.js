a7a89b1d654368fc42c010ea6589548852ad47a4
takeshisakuma <saude0518@gmail.com>
2026-01-31 22:12:54 +0900

    Updates

---
+++ assets/Icon.stories-EEoIHja0.js
@@ -0,0 +1,19 @@
+import{I as a}from"./Icon-CalW3yg7.js";import"./jsx-runtime-u17CrQMm.js";import"./index-BhPnCaJW.js";import"./iframe-CN4VAW7m.js";import"./preload-helper-PPVm8Dsz.js";const{fn:n}=__STORYBOOK_MODULE_TEST__,d={title:"Component/Typography & Base/Icon",component:a,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{color:{control:{type:"select"},options:["destructive","positive","caution","informative","primary","secondary","tertiary","disabled"]}},args:{onClick:n(),name:"CircleIcon",size:"medium",color:"destructive"}},e={args:{name:"SquareIcon",size:"medium",color:"primary"}},r={args:{name:"CircleIcon",size:"large",color:"destructive"}},o={args:{name:"LoadingIcon",size:"large",color:"primary"}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    name: "SquareIcon",
+    size: "medium",
+    color: "primary"
+  }
+}`,...e.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    name: "CircleIcon",
+    size: "large",
+    color: "destructive"
+  }
+}`,...r.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    name: "LoadingIcon",
+    size: "large",
+    color: "primary"
+  }
+}`,...o.parameters?.docs?.source}}};const p=["MediumSquareIcon","MediumCircleIcon","MediumLoadingIcon"];export{r as MediumCircleIcon,o as MediumLoadingIcon,e as MediumSquareIcon,p as __namedExportsOrder,d as default};
--- assets/formatter-EIJCOSYU-BHuDJGw6.js
+++
@@ -1,1 +0,0 @@
-import{e as m,r as o}from"./blocks-D5SAiiPI.js";import{b as t}from"./iframe-DKu933ra.js";import"./preload-helper-PPVm8Dsz.js";import"./index-D5EulzDT.js";import"./jsx-runtime-u17CrQMm.js";import"./index-BEkOikoo.js";var e=t(o(),1),c=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{c as formatter};
