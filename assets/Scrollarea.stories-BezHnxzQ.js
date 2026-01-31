1a5b545620f4885d1e44cc1c4a0ce9d0df086ecb
takeshisakuma <saude0518@gmail.com>
2026-01-31 10:53:52 +0900

    Updates

--- assets/Scrollarea.stories-BezHnxzQ.js
+++
@@ -1,7 +0,0 @@
-import{j as l}from"./jsx-runtime-u17CrQMm.js";import{P as r}from"./index-rZgw565B.js";import{u as i}from"./useTranslation-CpmShhJX.js";import"./iframe-Dm9h3SgM.js";import"./preload-helper-PPVm8Dsz.js";import"./index-BBw2F7dE.js";const s=({text:a,scrollAxis:n="y",maxHeight:t="10rem",...m})=>{const{t:o}=i();return l.jsx("div",{className:"wim-scrollarea",style:{maxHeight:t},children:o(a)})};s.propTypes={text:r.string.isRequired,scrollAxis:r.oneOf(["y"]),maxHeight:r.string};s.__docgenInfo={description:"",methods:[],displayName:"Scrollarea",props:{text:{required:!0,tsType:{name:"string"},description:"",type:{name:"string"}},scrollAxis:{required:!1,tsType:{name:"literal",value:'"y"'},description:"",defaultValue:{value:'"y"',computed:!1},type:{name:"enum",value:[{value:'"y"',computed:!1}]}},maxHeight:{required:!1,tsType:{name:"string"},description:"Maximum height of the scroll area",type:{name:"string"}}}};const g={title:"Component/Navigation & Layout/Scrollarea",component:s,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{}},e={args:{text:"sampletextforscrollarea",scrollAxis:"y",maxHeight:"10rem"}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {
-    text: "sampletextforscrollarea",
-    scrollAxis: \`y\`,
-    maxHeight: \`10rem\`
-  }
-}`,...e.parameters?.docs?.source}}};const f=["ScrollareaYaxis"];export{e as ScrollareaYaxis,f as __namedExportsOrder,g as default};
--- assets/formatter-EIJCOSYU-Cm1UvlmR.js
+++
@@ -1,1 +0,0 @@
-import{e as m,r as o}from"./blocks-CbT7-naF.js";import{b as t}from"./iframe-Dm9h3SgM.js";import"./preload-helper-PPVm8Dsz.js";import"./index-BRMMBTls.js";import"./jsx-runtime-u17CrQMm.js";import"./index-BBw2F7dE.js";var e=t(o(),1),c=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{c as formatter};
