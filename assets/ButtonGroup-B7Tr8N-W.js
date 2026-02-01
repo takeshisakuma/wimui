6f64c2745f44a961f103ebd952cd5a81e1010e9d
takeshisakuma <saude0518@gmail.com>
2026-02-01 22:13:55 +0900

    Updates

---
+++ assets/ButtonGroup-B7Tr8N-W.js
@@ -0,0 +1,20 @@
+import{j as n}from"./jsx-runtime-u17CrQMm.js";import{useMDXComponents as t}from"./index-bLgR9I4r.js";import{M as d,T as m,S as a,D as c,a as e,C as p}from"./index-CI9naYhi.js";import{B as x,M as s,L as j,S as l,J as h}from"./ButtonGroup.stories-BFxtQqyh.js";import"./T-W2IAkAT2.js";import"./iframe-jjOh1F7M.js";import"./preload-helper-PPVm8Dsz.js";import"./index-Bfc_VBjE.js";import"./index-DDjLOUk7.js";import"./index-D9xBpP30.js";import"./index-Cq7QWv9l.js";import"./Button-B2IkHhyK.js";import"./Icon-rXCHR63U.js";import"./useTranslation-iuooOi5_.js";function i(r){const o={code:"code",h2:"h2",h3:"h3",p:"p",...t(),...r.components};return n.jsxs(n.Fragment,{children:[n.jsx(d,{of:x}),`
+`,n.jsx(m,{}),`
+`,n.jsx(a,{children:n.jsx(o.p,{children:"ButtonGroupコンポーネントは、複数のボタンをグループ化して表示するために使用します。"})}),`
+`,n.jsx(c,{}),`
+`,n.jsx(o.h2,{id:"基本的な使い方",children:"基本的な使い方"}),`
+`,n.jsxs(o.p,{children:["ボタンを横並びに配置します。",n.jsx(o.code,{children:"gap"}),"プロパティでボタン間の間隔を調整できます。"]}),`
+`,n.jsx(e,{of:s}),`
+`,n.jsx(o.h2,{id:"サイズ展開",children:"サイズ展開"}),`
+`,n.jsx(o.p,{children:"Large, Medium, Smallのボタンをそれぞれ配置した場合の例です。"}),`
+`,n.jsx(o.h3,{id:"large",children:"Large"}),`
+`,n.jsx(e,{of:j}),`
+`,n.jsx(o.h3,{id:"medium",children:"Medium"}),`
+`,n.jsx(e,{of:s}),`
+`,n.jsx(o.h3,{id:"small",children:"Small"}),`
+`,n.jsx(e,{of:l}),`
+`,n.jsx(o.h2,{id:"結合モード",children:"結合モード"}),`
+`,n.jsxs(o.p,{children:[n.jsx(o.code,{children:"joined"}),"プロパティを",n.jsx(o.code,{children:"true"}),`に設定すると、ボタンが隙間なく結合されます。\r
+このモードでは、中間のボタンの角丸が削除され、一体感のあるデザインになります。`]}),`
+`,n.jsx(e,{of:h}),`
+`,n.jsx(p,{})]})}function b(r={}){const{wrapper:o}={...t(),...r.components};return o?n.jsx(o,{...r,children:n.jsx(i,{...r})}):i(r)}export{b as default};
