4a1b9d2c487c63823bbd7e58495d86d36ca6341d
takeshisakuma <saude0518@gmail.com>
2026-02-01 13:08:08 +0900

    Updates

---
+++ assets/SegmentedControl-DmwRiWLD.js
@@ -0,0 +1,26 @@
+import{j as n}from"./jsx-runtime-u17CrQMm.js";import{useMDXComponents as t}from"./index-BIyE10jU.js";import{M as a,T as x,S as d,D as j,C as i,a as l}from"./blocks-DWXTubRM.js";import{S as m,M as r,a as h,L as c,W as p,I as f,T as u}from"./SegmentedControl.stories-Bx0PAjlE.js";import"./T-YvwJYx0g.js";import"./iframe-CzUuUlbd.js";import"./preload-helper-PPVm8Dsz.js";import"./index-BpO35oC8.js";import"./index-C_r40EIC.js";import"./index-BgUr35ih.js";import"./Icon-IErQSuUS.js";import"./useTranslation-CQkX1ujI.js";function o(e){const s={h2:"h2",h3:"h3",p:"p",...t(),...e.components};return n.jsxs(n.Fragment,{children:[n.jsx(a,{of:m}),`
+`,n.jsx(x,{}),`
+`,n.jsx(d,{children:n.jsx(s.p,{children:"複数の選択肢の中から1つを選択するために使用"})}),`
+`,n.jsx(j,{}),`
+`,n.jsx(s.h2,{id:"基本的な使い方",children:"基本的な使い方"}),`
+`,n.jsx(s.p,{children:"排他的な選択（ラジオボタンのような挙動）を行う際に適す"}),`
+`,n.jsx(i,{of:r}),`
+`,n.jsx(s.h2,{id:"サイズ展開",children:"サイズ展開"}),`
+`,n.jsx(s.p,{children:"Small, Medium, Largeのサイズバリエーションがある"}),`
+`,n.jsx(s.h3,{id:"small",children:"Small"}),`
+`,n.jsx(i,{of:h}),`
+`,n.jsx(s.h3,{id:"medium",children:"Medium"}),`
+`,n.jsx(i,{of:r}),`
+`,n.jsx(s.h3,{id:"large",children:"Large"}),`
+`,n.jsx(i,{of:c}),`
+`,n.jsx(s.h2,{id:"アイコンの利用",children:"アイコンの利用"}),`
+`,n.jsx(s.p,{children:"アイコンとラベルを組み合わせて使用したり、アイコンのみを表示したりすることが可能"}),`
+`,n.jsx(s.h3,{id:"アイコン--ラベル",children:"アイコン + ラベル"}),`
+`,n.jsx(i,{of:p}),`
+`,n.jsx(s.h3,{id:"アイコンのみ",children:"アイコンのみ"}),`
+`,n.jsx(i,{of:f}),`
+`,n.jsx(s.h2,{id:"選択肢が2つの場合",children:"選択肢が2つの場合"}),`
+`,n.jsxs(s.p,{children:["2つの選択肢(On/OffやYes/Noなど)でも利用可能",n.jsx("br",{}),`\r
+選択肢が2つの場合に、デザインによってどちらが選択されているのかわからなくならないようにする`]}),`
+`,n.jsx(i,{of:u}),`
+`,n.jsx(l,{})]})}function y(e={}){const{wrapper:s}={...t(),...e.components};return s?n.jsx(s,{...e,children:n.jsx(o,{...e})}):o(e)}export{y as default};
