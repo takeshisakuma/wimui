72c11f3dd1974651721af3503e55df20c2898b86
takeshisakuma <saude0518@gmail.com>
2026-02-01 22:00:42 +0900

    Updates

---
+++ assets/Icon.stories-BO4JvYKu.js
@@ -0,0 +1,25 @@
+import{I as n}from"./Icon-y5cr71Py.js";import"./jsx-runtime-u17CrQMm.js";import"./index-DOdG3W7E.js";import"./iframe-HL8_cyCC.js";import"./preload-helper-PPVm8Dsz.js";const{fn:s}=__STORYBOOK_MODULE_TEST__,p={title:"Component/Actions/Icon",component:n,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{color:{control:{type:"select"},options:["destructive","positive","caution","informative","primary","secondary","tertiary","disabled"]}},args:{onClick:s(),name:"CircleIcon",size:"medium",color:"destructive"}},e={args:{name:"SquareIcon",size:"medium",color:"primary"}},r={args:{name:"CircleIcon",size:"large",color:"destructive"}},o={args:{name:"LoadingIcon",size:"large",color:"primary"}},a={args:{name:"ExternalLinkIcon",size:"medium",color:"primary"}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    name: "SquareIcon",
+    size: "medium",
+    color: "primary"
+  }
+}`,...e.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    name: "CircleIcon",
+    size: "large",
+    color: "destructive"
+  }
+}`,...r.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    name: "LoadingIcon",
+    size: "large",
+    color: "primary"
+  }
+}`,...o.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    name: "ExternalLinkIcon",
+    size: "medium",
+    color: "primary"
+  }
+}`,...a.parameters?.docs?.source}}};const u=["MediumSquareIcon","MediumCircleIcon","MediumLoadingIcon","MediumExternalLinkIcon"];export{r as MediumCircleIcon,a as MediumExternalLinkIcon,o as MediumLoadingIcon,e as MediumSquareIcon,u as __namedExportsOrder,p as default};
--- assets/formatter-EIJCOSYU-DvyPsYB4.js
+++
@@ -1,1 +0,0 @@
-import{d as m,r as o}from"./index-Cu96thwa.js";import{_ as t}from"./iframe-bapbJR6E.js";import"./preload-helper-PPVm8Dsz.js";import"./index-CuYIkxca.js";import"./jsx-runtime-u17CrQMm.js";import"./index-DfC4URaJ.js";var e=t(o(),1),c=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{c as formatter};
