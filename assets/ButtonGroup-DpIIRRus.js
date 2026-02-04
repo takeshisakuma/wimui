305aea853d7f95e2c03dbbbe3b4d77f4d4cbd5b6
takeshisakuma <saude0518@gmail.com>
2026-02-04 22:46:41 +0900

    Updates

---
+++ assets/ButtonGroup-DpIIRRus.js
@@ -0,0 +1,24 @@
+import{j as r}from"./jsx-runtime-u17CrQMm.js";import{useMDXComponents as d}from"./index-BtdJRefg.js";import{M as t,T as c,S as j,D as x,C as i,a}from"./index-BCzhjH4V.js";import{B as l,M as o,L as p,S as m,J as h,a as u}from"./ButtonGroup.stories-D_lxpvYA.js";import"./T-DTgS86jj.js";import"./iframe-UAeCG2Vk.js";import"./preload-helper-PPVm8Dsz.js";import"./index-D23l0ZLc.js";import"./index-dZZVaWsT.js";import"./index-B_wKiU58.js";import"./index-DDsuntTE.js";import"./Button-CVBlOQjd.js";import"./Icon-D0U75PM1.js";import"./useTranslation-C9Cx9JhI.js";function s(e){const n={code:"code",h2:"h2",h3:"h3",p:"p",...d(),...e.components};return r.jsxs(r.Fragment,{children:[r.jsx(t,{of:l}),`
+`,r.jsx(c,{}),`
+`,r.jsx(j,{children:r.jsx(n.p,{children:"ButtonGroupコンポーネントは、複数のボタンをグループ化して表示するために使用します。"})}),`
+`,r.jsx(x,{}),`
+`,r.jsx(n.h2,{id:"基本的な使い方",children:"基本的な使い方"}),`
+`,r.jsxs(n.p,{children:["ボタンを横並びに配置します。",r.jsx(n.code,{children:"gap"}),"プロパティでボタン間の間隔を調整できます。"]}),`
+`,r.jsx(i,{of:o}),`
+`,r.jsx(n.h2,{id:"サイズ展開",children:"サイズ展開"}),`
+`,r.jsx(n.p,{children:"Large, Medium, Smallのボタンをそれぞれ配置した場合の例です。"}),`
+`,r.jsx(n.h3,{id:"large",children:"Large"}),`
+`,r.jsx(i,{of:p}),`
+`,r.jsx(n.h3,{id:"medium",children:"Medium"}),`
+`,r.jsx(i,{of:o}),`
+`,r.jsx(n.h3,{id:"small",children:"Small"}),`
+`,r.jsx(i,{of:m}),`
+`,r.jsx(n.h2,{id:"結合モード",children:"結合モード"}),`
+`,r.jsxs(n.p,{children:[r.jsx(n.code,{children:"joined"}),"プロパティを",r.jsx(n.code,{children:"true"}),`に設定すると、ボタンが隙間なく結合されます。\r
+このモードでは、中間のボタンの角丸が削除され、一体感のあるデザインになります。`]}),`
+`,r.jsx(i,{of:h}),`
+`,r.jsx(n.h3,{id:"優先度の強制-priority-override",children:"優先度の強制 (Priority Override)"}),`
+`,r.jsxs(n.p,{children:[r.jsx(n.code,{children:"priority"})," プロパティを指定することで、グループ内のすべてのボタンの ",r.jsx(n.code,{children:"priority"}),` を統一できます。\r
+特に `,r.jsx(n.code,{children:"joined"})," モードを使用する場合、異なる優先度のボタンが混在するとデザインが崩れる可能性があるため、このプロパティを使用して ",r.jsx(n.code,{children:"secondary"})," や ",r.jsx(n.code,{children:"primary"})," に統一することを推奨します。"]}),`
+`,r.jsx(i,{of:u}),`
+`,r.jsx(a,{})]})}function T(e={}){const{wrapper:n}={...d(),...e.components};return n?r.jsx(n,{...e,children:r.jsx(s,{...e})}):s(e)}export{T as default};
---
+++ assets/Quote-CW4aWbX6.js
@@ -0,0 +1,10 @@
+import{j as t}from"./jsx-runtime-u17CrQMm.js";import{useMDXComponents as e}from"./index-BtdJRefg.js";import{M as i,T as a,S as m,D as x,C as s,a as p}from"./index-BCzhjH4V.js";import{Q as j,D as c,W as f,L as u,N as l}from"./Quote.stories-C4T49uus.js";import"./iframe-UAeCG2Vk.js";import"./preload-helper-PPVm8Dsz.js";import"./index-D23l0ZLc.js";import"./index-dZZVaWsT.js";import"./index-B_wKiU58.js";import"./index-DDsuntTE.js";import"./useTranslation-C9Cx9JhI.js";function r(o){const n={p:"p",...e(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(i,{of:j}),`
+`,t.jsx(a,{}),`
+`,t.jsx(m,{children:t.jsx(n.p,{children:"引用文や強調したいテキストを表示するコンポーネント"})}),`
+`,t.jsx(x,{}),`
+`,t.jsx("h2",{children:"バリエーション"}),`
+`,t.jsx(s,{of:c}),`
+`,t.jsx(s,{of:f}),`
+`,t.jsx(s,{of:u}),`
+`,t.jsx(s,{of:l}),`
+`,t.jsx(p,{})]})}function T(o={}){const{wrapper:n}={...e(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(r,{...o})}):r(o)}export{T as default};
