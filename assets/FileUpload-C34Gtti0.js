3260a5b6b94523a4703c1106955406eb6bdfc19f
takeshisakuma <saude0518@gmail.com>
2026-02-04 23:12:43 +0900

    Updates

---
+++ assets/FileUpload-C34Gtti0.js
@@ -0,0 +1,17 @@
+import{j as n}from"./jsx-runtime-u17CrQMm.js";import{useMDXComponents as i}from"./index-3Ej3eRcy.js";import{M as r,T as c,S as d,C as o,a as l}from"./index-CAUYrQbg.js";import{F as x,D as j,M as a,A as h,a as p}from"./FileUpload.stories-DOOaa0ii.js";import"./iframe-b50X8cdi.js";import"./preload-helper-PPVm8Dsz.js";import"./index-DS5rv49I.js";import"./index-DOKrV1tN.js";import"./index-Bx_oMoPf.js";import"./index-B70P5TEQ.js";import"./Button-CdXGD-HQ.js";import"./Icon-B31ltcqP.js";import"./useTranslation-CF2pUroq.js";function t(s){const e={code:"code",h2:"h2",h3:"h3",p:"p",...i(),...s.components};return n.jsxs(n.Fragment,{children:[n.jsx(r,{of:x}),`
+`,n.jsx(c,{}),`
+`,n.jsx(d,{children:n.jsx(e.p,{children:"ユーザーがローカルファイルを一つ以上選択してアップロードするために使用されます。"})}),`
+`,n.jsx(e.h2,{id:"使用法",children:"使用法"}),`
+`,n.jsxs(e.p,{children:[n.jsx(e.code,{children:"Button"})," コンポーネントをトリガーとしてファイル選択ダイアログを開きます。"]}),`
+`,n.jsx(o,{of:j}),`
+`,n.jsx(l,{}),`
+`,n.jsx(e.h2,{id:"バリエーション",children:"バリエーション"}),`
+`,n.jsx(e.h3,{id:"複数ファイル選択",children:"複数ファイル選択"}),`
+`,n.jsxs(e.p,{children:[n.jsx(e.code,{children:"multiple"})," プロパティを ",n.jsx(e.code,{children:"true"})," に設定することで、複数のファイルを選択できます。"]}),`
+`,n.jsx(o,{of:a}),`
+`,n.jsx(e.h3,{id:"特定のファイル形式のみ許可",children:"特定のファイル形式のみ許可"}),`
+`,n.jsxs(e.p,{children:[n.jsx(e.code,{children:"accept"})," プロパティを使用して、選択可能なファイル形式を制限できます。"]}),`
+`,n.jsx(o,{of:h}),`
+`,n.jsx(e.h3,{id:"無効状態",children:"無効状態"}),`
+`,n.jsxs(e.p,{children:[n.jsx(e.code,{children:"disabled"})," プロパティを ",n.jsx(e.code,{children:"true"})," に設定することで、操作を無効にします。"]}),`
+`,n.jsx(o,{of:p})]})}function _(s={}){const{wrapper:e}={...i(),...s.components};return e?n.jsx(e,{...s,children:n.jsx(t,{...s})}):t(s)}export{_ as default};
