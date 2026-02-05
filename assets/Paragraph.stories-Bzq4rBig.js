2fc725901eaf3aa6781948c76a851b5d1e035e22
takeshisakuma <saude0518@gmail.com>
2026-02-05 23:08:21 +0900

    Updates

---
+++ assets/Paragraph.stories-Bzq4rBig.js
@@ -0,0 +1,19 @@
+import{P as g}from"./Paragraph-Bxbd9UCr.js";import"./jsx-runtime-u17CrQMm.js";import"./index-98ZzNgXr.js";import"./iframe-C9O3I54Q.js";import"./preload-helper-PPVm8Dsz.js";import"./useTranslation-Ba8LUUji.js";import"./index-B6bUO9K-.js";const{fn:i}=__STORYBOOK_MODULE_TEST__,_={title:"Component/Typography/Paragraph",component:g,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{},args:{onClick:i()}},a={args:{size:"ex-large",content:"ex-large_paragraph",weight:"normal",lineHeight:"normal-latn",style:"normal"}},r={args:{size:"large",content:"large_paragraph",weight:"normal",lineHeight:"normal-latn",style:"normal"}},e={args:{size:"medium",content:"medium_paragraph",weight:"normal",lineHeight:"normal-latn",style:"normal"}},n={args:{size:"small",content:"small_paragraph",weight:"normal",lineHeight:"normal-latn",style:"normal"}},t={args:{size:"ex-small",content:"ex-small_paragraph",weight:"normal",lineHeight:"normal-latn",style:"normal"}},o={args:{size:"medium",content:"Bold Paragraph",weight:"bold"}},s={args:{size:"medium",content:"Strikethrough Paragraph",decoration:"line-through"}},l={args:{size:"medium",content:"Highlight Paragraph",decoration:"highlight"}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:'{\n  args: {\n    size: "ex-large",\n    content: `ex-large_paragraph`,\n    weight: `normal`,\n    lineHeight: `normal-latn`,\n    style: `normal`\n  }\n}',...a.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:'{\n  args: {\n    size: "large",\n    content: `large_paragraph`,\n    weight: `normal`,\n    lineHeight: `normal-latn`,\n    style: `normal`\n  }\n}',...r.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:'{\n  args: {\n    size: "medium",\n    content: `medium_paragraph`,\n    weight: `normal`,\n    lineHeight: `normal-latn`,\n    style: `normal`\n  }\n}',...e.parameters?.docs?.source}}};n.parameters={...n.parameters,docs:{...n.parameters?.docs,source:{originalSource:'{\n  args: {\n    size: "small",\n    content: `small_paragraph`,\n    weight: `normal`,\n    lineHeight: `normal-latn`,\n    style: `normal`\n  }\n}',...n.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:'{\n  args: {\n    size: "ex-small",\n    content: `ex-small_paragraph`,\n    weight: `normal`,\n    lineHeight: `normal-latn`,\n    style: `normal`\n  }\n}',...t.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    size: "medium",
+    content: "Bold Paragraph",
+    weight: "bold"
+  }
+}`,...o.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    size: "medium",
+    content: "Strikethrough Paragraph",
+    decoration: "line-through"
+  }
+}`,...s.parameters?.docs?.source}}};l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  args: {
+    size: "medium",
+    content: "Highlight Paragraph",
+    decoration: "highlight"
+  }
+}`,...l.parameters?.docs?.source}}};const S=["ExLargeParagraph","LargeParagraph","MediumParagraph","SmallParagrapht","ExSmallParagraph","BoldParagraph","StrikethroughParagraph","HighlightParagraph"];export{o as BoldParagraph,a as ExLargeParagraph,t as ExSmallParagraph,l as HighlightParagraph,r as LargeParagraph,e as MediumParagraph,n as SmallParagrapht,s as StrikethroughParagraph,S as __namedExportsOrder,_ as default};
---
+++ assets/imagesanple-VDI2Kzk_.js
@@ -0,0 +1,1 @@
+const e=""+new URL("imagesanple-j58eW6sL.webp",import.meta.url).href;export{e as s};
