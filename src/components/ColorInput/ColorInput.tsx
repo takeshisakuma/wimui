395fbd96cf001019fb39225a876aa9484abf99cd
takeshisakuma <saude0518@gmail.com>
2026-02-17 21:14:41 +0900

    fix ColorInput Component

--- src/components/ColorInput/ColorInput.tsx
+++ src/components/ColorInput/ColorInput.tsx
@@ -1,13 +1,15 @@
 import React from "react";
+import classNames from "classnames";
 import { Input } from "../Input/Input";
 
 /**
  * カラー選択に特化した入力コンポーネント。
  */
-export const ColorInput = (props: React.ComponentProps<typeof Input>) => {
+export const ColorInput = ({ className, ...props }: React.ComponentProps<typeof Input>) => {
     return (
         <Input
             type="color"
+            className={classNames("wim-color-input", className)}
             {...props}
         />
     );
--- stories/ColorInput/ColorInput.stories.tsx
+++ stories/ColorInput/ColorInput.stories.tsx
@@ -16,4 +16,21 @@ export const Default: Story = {
     },
 };
 
+export const WithIcon: Story = {
+    args: {
+        defaultValue: "#3b82f6",
+        leftIcon: "ImageIcon",
+    },
+};
+
+export const CustomStates: Story = {
+    render: (args) => (
+        <div style={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
+            <ColorInput {...args} state="default" defaultValue="#10b981" leftIcon="CheckCircleIcon" />
+            <ColorInput {...args} state="error" defaultValue="#ef4444" leftIcon="AlertCircleIcon" />
+            <ColorInput {...args} state="disabled" defaultValue="#6b7280" leftIcon="SettingsIcon" />
+        </div>
+    ),
+};
+
 
